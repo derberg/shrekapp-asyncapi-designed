64afa31bdc87fdb61a9004c577718db54782fbd1
derberg <lpgornicki@gmail.com>
2021-04-29 16:03:13 +0200

    feat: add business logic to travel/status

--- src/api/services/travel-status.js
+++ src/api/services/travel-status.js
@@ -1,9 +1,24 @@
 const service = module.exports = {};
+const dummyjson = require('dummy-json');
 
 /**
  * Client can receive travel info status.
  * @param {object} ws WebSocket connection.
  */
 service.subTravelInfo = async (ws) => {
-  ws.send('Message from the server: Implement here your business logic that sends messages to a client after it connects.');
+  (function myLoop (i) {
+    setTimeout(() => {
+      ws.send(generateResponse());
+      if (--i) myLoop(i);
+    }, 1000);
+  }(100));  
+
+  function generateResponse() {
+    const template = `{
+      "destination": "{{city}}",
+      "arrival": "{{int 2 6}}h",
+      "distance": "{{int 18 65}}km"
+    }`;
+    return dummyjson.parse(template);
+  }
 };
